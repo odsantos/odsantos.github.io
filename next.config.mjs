8d8c688aad1fede59317f1743fd297405156f065
Osvaldo Dias dos Santos <osvaldo.msdsantos@yahoo.com>
2025-09-25 21:00:16 +0100

    feat: Configure Next.js for static export and add GitHub Actions deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Enable static exports
+  output: 'export',
+
+  // Disable image optimization, as it is not supported on static hosts
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
