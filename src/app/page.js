907da53a93d903b61040f28f4f133ff33ff22700
Osvaldo Dias dos Santos <osvaldo.msdsantos@yahoo.com>
2025-09-26 00:38:08 +0100

    feat: Integrate Google Forms contact form

--- src/app/page.js
+++ src/app/page.js
@@ -121,33 +121,7 @@ export default function Home() {
               <p className="lead text-center mb-4">
                 Have a question or want to work together?
               </p>
-              {/* Note: This is a static form and does not submit data. */}
-              <form>
-                <div className="form-group mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Your Name"
-                  />
-                </div>
-                <div className="form-group mb-3">
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Your Email"
-                  />
-                </div>
-                <div className="form-group mb-3">
-                  <textarea
-                    className="form-control"
-                    rows="5"
-                    placeholder="Your Message"
-                  ></textarea>
-                </div>
-                <button type="submit" className="btn btn-primary w-100">
-                  Submit
-                </button>
-              </form>
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc-TAzxFemO5a39d3PDlRrsqzq50TxJ-bNICVgu9_tXxphSg/viewform?embedded=true" width="640" height="741" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </div>
           </div>
         </Container>
